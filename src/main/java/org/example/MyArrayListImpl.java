887bf2d885a3200122e43c8ea6276d8a6752fd9a
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-02-13 18:35:35 +0900

    メソッドの修正

--- src/main/java/org/example/MyArrayListImpl.java
+++ src/main/java/org/example/MyArrayListImpl.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Arrays;
 
-public class MyArrayListImpl<E> implements MyArrayList<E>{
+public class MyArrayListImpl<E> implements MyList<E>{
     private Object[] elements;
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
@@ -12,45 +12,61 @@ public class MyArrayListImpl<E> implements MyArrayList<E>{
     }
 
     public boolean add(E e) {
-        if(size == elements.length) { ensureCapacity(); }
-        elements[size++] = e;
+        if(size == elements.length) ensureCapacity();
+        elements[size] = e;
+        size++;
         return true;
     };
 
-    public void add(int index, E e) {
+    public void add(int index, E e) throws ArrayIndexOutOfBoundsException {
+        if(size == elements.length) ensureCapacity();
+
+        Object[] behindIndexList = Arrays.copyOfRange(elements, index, size);
         elements[index] = e;
+
+        System.arraycopy(behindIndexList, 0, elements, index + 1, behindIndexList.length);
+
+        size++;
     };
 
     public boolean contains(Object o) {
         return Arrays.stream(elements)
             .filter(el -> {
-                if(el == null) {return false;}
+                if(el == null) return false;
                 return el.equals(o);
             })
             .map(el -> true)
             .findFirst().orElse(false);
     };
 
-    public E get(int index) {
+    public E get(int index) throws ArrayIndexOutOfBoundsException {
         return (E) elements[index];
     };
 
-    public E remove(int index) {
-        E val = (E)elements[index];
-        elements[index] = null;
+    public E remove(int index) throws ArrayIndexOutOfBoundsException {
+        var val = (E)elements[index];
+
+        var newList = new Object[elements.length];
+        var beforeIndexList = Arrays.copyOfRange(elements, 0, index);
+        var afterIndexList = Arrays.copyOfRange(elements, index + 1, size);
+        System.arraycopy(beforeIndexList, 0, newList, 0, beforeIndexList.length);
+        System.arraycopy(afterIndexList, 0, newList, index, afterIndexList.length);
+
+        size--;
+        this.elements = newList;
         return val;
     };
 
     public Object[] toArray() {
-        return elements;
+        return Arrays.copyOfRange(elements, 0, size);
     };
 
     public int size() {
-        return elements.length;
+        return size;
     }
 
     private void ensureCapacity() {
-        int newSize = elements.length * 2;
+        var newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
 }
