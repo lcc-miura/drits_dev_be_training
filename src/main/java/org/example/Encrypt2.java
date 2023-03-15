8de67a0c27b10f158b46440b5a5c96642e191785
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-03-15 17:33:54 +0900

    修正

--- src/main/java/org/example/Encrypt2.java
+++ src/main/java/org/example/Encrypt2.java
@@ -5,40 +5,22 @@ import java.util.List;
 
 public class Encrypt2 implements Encrypt {
 
-    private final static List<String> alphabets = Arrays.asList("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
-    private final static List<String> LowerAlphabets = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
-
     public String encrypt( String s ) {
 
         return Arrays.stream(s.split(""))
             .map(str -> {
-
-                if(str.matches("[0-9]"))
-                    return !str.equals("9")
-                        ? increment(str)
-                        : "0";
-
-                if(str.matches("[A-Z]")) {
-                    return !str.equals("Z")
-                        ? alphabets.get(alphabets.indexOf(str) + 1)
-                        : "A";
-                }
-
-                if(str.matches("[a-z]")) {
-                    return !str.equals("z")
-                        ? LowerAlphabets.get(LowerAlphabets.indexOf(str) + 1)
-                        : "a";
-                }
-
+                if(str.matches("[0-9]")) return encryptUpper(str, 48, 10);
+                if(str.matches("[A-Z]")) return encryptUpper(str, 65, 26);
+                if(str.matches("[a-z]")) return encryptUpper(str, 97, 26);
                 return str;
             })
             .reduce((result, str) -> result + str)
             .orElse("");
     }
 
-    private String increment(String s) {
-        var n = Integer.parseInt(s);
-        n++;
-        return String.valueOf(n);
+    public String encryptUpper(String str, int baseNum, int count){
+        var c = str.toCharArray()[0];
+        var result =  (char)(baseNum + (c - baseNum + 1) % count);
+        return String.valueOf(result);
     }
 }
