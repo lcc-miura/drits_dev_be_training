aa3284018050ce9e270cc02e3c9e557f8554a8f1
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-03-15 17:56:37 +0900

    修正

--- src/main/java/org/example/Encrypt2.java
+++ src/main/java/org/example/Encrypt2.java
@@ -9,16 +9,16 @@ public class Encrypt2 implements Encrypt {
 
         return Arrays.stream(s.split(""))
             .map(str -> {
-                if(str.matches("[0-9]")) return encryptUpper(str, 48, 10);
-                if(str.matches("[A-Z]")) return encryptUpper(str, 65, 26);
-                if(str.matches("[a-z]")) return encryptUpper(str, 97, 26);
+                if(str.matches("[0-9]")) return toNextChar(str, '0', 10);
+                if(str.matches("[A-Z]")) return toNextChar(str, 'A', 26);
+                if(str.matches("[a-z]")) return toNextChar(str, 'a', 26);
                 return str;
             })
             .reduce((result, str) -> result + str)
             .orElse("");
     }
 
-    public String encryptUpper(String str, int baseNum, int count){
+    public String toNextChar(String str, int baseNum, int count){
         var c = str.toCharArray()[0];
         var result =  (char)(baseNum + (c - baseNum + 1) % count);
         return String.valueOf(result);
