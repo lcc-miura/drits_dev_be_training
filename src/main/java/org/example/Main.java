098718476879cf9273814c6a27a202d419d04462
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-03-16 13:36:47 +0900

    不要な記述を削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,33 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        MyArrayListImpl<String> myArrayList = new MyArrayListImpl<String>();
 
-        myArrayList.add("val1");
-        myArrayList.add("val2");
-        myArrayList.add("val3");
-        myArrayList.add("val4");
-        myArrayList.add("val5");
-        myArrayList.add("val6");
-        myArrayList.add("val7");
-        myArrayList.add("val8");
-        myArrayList.add("val9");
-        myArrayList.add("val10");
-        myArrayList.add("val11");
+        char []alphabet = new char[26];
+        char c = 'a';
+        for (int i = 0; i < 26; i++){
+            alphabet[i] = c++;
+            System.out.println((int)alphabet[i]);
+        }
 
-        myArrayList.add(0, "newVal1");
+//        char c = 'A';
+//        for (int i = 0; i < 26; i++){
+//            alphabet[i] = c++;
+//            System.out.println((int)alphabet[i]);
+//        }
 
-        boolean result1 = myArrayList.contains("newVal1");
-        boolean result2 = myArrayList.contains("noContainVal");
-        System.out.println("contains " + result1);
-        System.out.println("contains " + result2);
+//        char c = '0';
+//        for (int i = 0; i < 10; i++){
+//            alphabet[i] = c++;
+//            System.out.println((int)alphabet[i]);
+//        }
 
-        String  result3 = myArrayList.get(0);
-        System.out.println("get " + result3);
+        char[] hoge = "🍣".toCharArray();
+        System.out.println("🍣".length());
+        System.out.println(hoge[1]);
+        System.out.println(hoge[0]);
+        System.out.println(hoge[0] + hoge[1]);
+        System.out.println(String.valueOf(112543));
 
-        String  result4 = myArrayList.remove(0);
-        System.out.println("remove " + result4);
-
-        Object[] result5 = myArrayList.toArray();
-        System.out.println("toArray " + result5);
-
-        int result6 = myArrayList.size();
-        System.out.println("size " + result6);
     }
 }
