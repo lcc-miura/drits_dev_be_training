7b0d6818769d4f3afa2dfc5d4ac099a91ed9e0b2
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-03-16 16:31:19 +0900

    メンテナブルにする

--- src/main/java/org/example/Encrypt1.java
+++ src/main/java/org/example/Encrypt1.java
@@ -13,21 +13,31 @@ public class Encrypt1 implements Encrypt{
 
     public String encrypt( String s ){
 
+        ArrayList<List<String>> arr = generateArray(n);
+
         return Stream.iterate(
-            new IndexAndVal(0, String.valueOf( s.charAt(0) )),
-            iav -> new IndexAndVal(iav.index + 1, String.valueOf( s.charAt(iav.index + 1) ))
-        ).limit(s.length())
-        .reduce(
-            new ArrayList<List<String>>( Arrays.asList( new ArrayList<>(), new ArrayList<>(), new ArrayList<>() ) ),
-            (accum, iav) -> {
-                for (int i = 0; i < n; i++) {
-                    if(iav.index % n == i) accum.get(i).add(iav.val);
-                }
-                return accum;
-            },
-            (c1, c2) -> null
-        )
-        .stream().flatMap(Collection::stream)
-        .collect(Collectors.joining());
+                new IndexAndVal(0, String.valueOf( s.charAt(0) )),
+                iav -> new IndexAndVal(iav.index + 1, String.valueOf( s.charAt(iav.index + 1) ))
+            ).limit(s.length())
+            .reduce(
+                new ArrayList<>(arr),
+                (accum, iav) -> {
+                    for(int i = 0; i < n; i++) {
+                        if(iav.index % n == i) accum.get(i).add(iav.val);
+                    }
+                    return accum;
+                },
+                (c1, c2) -> null
+            )
+            .stream().flatMap(Collection::stream)
+            .collect(Collectors.joining());
+    }
+
+    private ArrayList<List<String>> generateArray(int n) {
+        ArrayList<List<String>> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(new ArrayList<>());
+        }
+        return arr;
     }
 }
