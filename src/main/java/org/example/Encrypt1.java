11d2d1eb5826e27ee85506b1ab0994854d72f36f
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-03-17 11:05:37 +0900

    修正

--- src/main/java/org/example/Encrypt1.java
+++ src/main/java/org/example/Encrypt1.java
@@ -1,40 +1,30 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Encrypt1 implements Encrypt{
     private final int n = 3;
-    private record IndexAndVal( int index, String val ){}
+    record PositionAndVal( int before, int after, char val ){}
 
-    public String encrypt( String s ){
-
-        var arr = generateArray(n);
-
-        return Stream.iterate(
-                new IndexAndVal(0, String.valueOf( s.charAt(0) )),
-                iav -> new IndexAndVal(iav.index + 1, String.valueOf( s.charAt(iav.index + 1) ))
-            ).limit(s.length())
-            .reduce(
-                new ArrayList<>(arr),
-                (accum, iav) -> {
-                    accum.get(iav.index % n).add(iav.val);
-                    return accum;
-                },
-                (c1, c2) -> null
-            )
-            .stream().flatMap(Collection::stream)
-            .collect(Collectors.joining());
+    private int getTransformPosition(int index, int length) {
+        return (index % n) * (length / n) + index / n;
     }
 
-    private List<List<String>> generateArray(int n) {
-        ArrayList<List<String>> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            arr.add(new ArrayList<>());
-        }
-        return arr;
+    public String encrypt( String s ) {
+
+        final int length = s.length();
+
+        return
+            Stream
+                .iterate(
+                    new PositionAndVal(0, 0, s.charAt(0)),
+                    pov -> new PositionAndVal(pov.before + 1, getTransformPosition(pov.before + 1, length), s.charAt(pov.before + 1))
+                )
+                .limit(length)
+                .sorted(Comparator.comparingInt(e -> e.after))
+                .map(el -> String.valueOf(el.val))
+                .collect(Collectors.joining());
     }
 }
